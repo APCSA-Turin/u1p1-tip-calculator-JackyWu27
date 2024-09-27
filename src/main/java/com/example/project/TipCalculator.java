4b768aaeec0d2769a3a68a159c267b74546917cb
JackyWu27 <jackyw27@nycstudents.net>
2024-09-27 02:50:56 +0000

    pls wrk

--- src/main/java/com/example/project/TipCalculator.java
+++ src/main/java/com/example/project/TipCalculator.java
@@ -3,23 +3,17 @@ package com.example.project;
 public class TipCalculator {
     public static String calculateTip(int people, int percent, double cost) { 
         //DO NOT DELETE ANY OF THE CODE BELOW      
-        StringBuilder result = new StringBuilder();
-        people = 6; 
-        percent = 25;
-        cost = 52.27;              
-        double tip = (int) ((cost * percent / 100) * 100) / 100.00 ; 
-
+        StringBuilder result = new StringBuilder();          
+        double tip = (int) (((cost * percent / 100) * 100) + 0.5) / 100.00 ; 
         result.append("-------------------------------\n");
-        result.append("Total bill before tip: " + cost); //concatenate to this string. DO NOT ERASE AND REWRITE
-        result.append("Total percentage: " + percent);
-        result.append("Total tip: " + tip);
-        result.append("Total Bill with tip: " + (cost + tip));
-        result.append("Per person cost before tip: " + (int)((cost / people) * 100) / 100.00);
-        result.append("Tip per person: " + ((int)((tip / people) * 100) / 100.00));
-        result.append("Total cost per person: " + ((int)(((cost + tip) / people) * 100)) / 100.00);
+        result.append("Total bill before tip: " + "$" + cost + "\n"); //concatenate to this string. DO NOT ERASE AND REWRITE
+        result.append("Total percentage: " + percent + "%" + "\n");
+        result.append("Total tip: " + "$" + tip + "\n");
+        result.append("Total Bill with tip: " + "$" + (cost + tip) + "\n");
+        result.append("Per person cost before tip: " + "$" + (int)(((cost / people) * 100) + 0.5) / 100.00 + "\n");
+        result.append("Tip per person: " + "$" + ((int) (((tip / people) * 100) + 0.5) / 100.00) + "\n");
+        result.append("Total cost per person: " + "$" + (int) (((cost + tip) / people) * 100) / 100.00 + "\n") ;
         result.append("-------------------------------\n");
-
-
         return result.toString();
     }
      //TEST YOUR PROGRAM IN main
--- src/main/java/com/example/project/test.java
+++ src/main/java/com/example/project/test.java
@@ -2,11 +2,14 @@ package com.example.project;
 
 public class test {
     public static void main(String[] args) {
-        int percent = 25;
-        double cost = 52.27;          
-        System.out.println(cost * percent );
-        System.out.println(cost * percent / 100);
-        System.out.println((cost * percent / 100) * 100);
-        System.err.println((int) ((cost * percent / 100) * 100) / 100.00);
+        int people = 12;
+        int percent = 15;
+        double cost = 566.97;
+        double tip = (int) (((cost * percent / 100) * 100) + 0.5) / 100.00 ;
+        
+        System.out.println((int) ((((cost + tip) / people) * 100) + 0.5) / 100.00);
+        System.out.println(((((cost + tip) / people) * 100) + 0.5) );
+        System.out.println(((cost + tip) / people) * 100);
+        System.out.println((cost + tip) / people);
     }
 }
